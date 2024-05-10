6713ee5cb0edb03a40505cbe0b4a5b989b5bd5c4
erAlinaAhsan <alina@hyperzod.com>
2024-05-10 14:04:28 +0530

    till categories

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,10 +11,15 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function ($slug) {
+Route::get('posts/{post:slug}', function (Post $post) {
     // Find a post by its slug and pass it to a view called "post".  
 
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => $post
+    ]);
+});
+Route::get('categories/{category}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
